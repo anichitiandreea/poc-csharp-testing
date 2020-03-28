067b7d1528bbad7d5c464f6e8d9dac3088fc431f
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-03-28 10:51:59 +0200

    Add DatabaseContext

---
+++ UnitTesting/Data/DatabaseContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UnitTesting.Domains;
+
+namespace UnitTesting.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        DbSet<Question> Questions { get; set; }
+        DbSet<Answer> Answers { get; set; }
+        DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseIdentityColumns();
+
+            modelBuilder.Entity<Question>()
+                .HasMany(question => question.Answers)
+                .WithOne()
+                .HasForeignKey(answer => answer.QuestionId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(user => user.Questions)
+                .WithOne()
+                .HasForeignKey(question => question.UserId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(user => user.Answers)
+                .WithOne()
+                .HasForeignKey(answer => answer.UserId);
+        }
+    }
+}
--- UnitTesting/Startup.cs
+++ UnitTesting/Startup.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using UnitTesting.Data;
 
 namespace UnitTesting
 {
@@ -16,6 +14,7 @@ namespace UnitTesting
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DatabaseContext>(options => options.UseNpgsql("Server=192.168.0.52;Port=5432;Database=UnitTesting;User Id=postgres;Password=parola;"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
