179b40583ba67e8c6f5f7ee6c3c22f3f033ccfa4
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-03-28 20:48:45 +0200

    Create QuestionController with CRUD methods

---
+++ UnitTesting/Controllers/QuestionController.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnitTesting.Domains;
+using UnitTesting.Services.Interfaces;
+
+namespace UnitTesting.Controllers
+{
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        private readonly IQuestionService questionService;
+        public QuestionController(IQuestionService questionService)
+        {
+            this.questionService = questionService;
+        }
+
+        public async Task<IActionResult> GetAllAsync()
+        {
+            try
+            {
+                var questions = await questionService.GetAllAsync();
+
+                if(questions is null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(questions);
+            }
+            catch(Exception exception)
+            {
+                return BadRequest(exception);
+            }
+        }
+
+        public async Task<IActionResult> GetByIdAsync(Guid id)
+        {
+            try
+            {
+                var question = await questionService.GetByIdAsync(id);
+
+                if(question is null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(question);
+            }
+            catch(Exception exception)
+            {
+                return BadRequest(exception);
+            }
+        }
+
+        public async Task<IActionResult> CreateAsync(Question question)
+        {
+            try
+            {
+                await questionService.CreateAsync(question);
+
+                return StatusCode(201, question);
+
+            }
+            catch(Exception exception)
+            {
+                return BadRequest(exception);
+            }
+        }
+
+        public async Task<IActionResult> CreateBulkAsync(IList<Question> questions)
+        {
+            try
+            {
+                await questionService.CreateBulkAsync(questions);
+
+                return StatusCode(201, questions);
+            }
+            catch(Exception exception)
+            {
+                return BadRequest(exception);
+            }
+        }
+
+        public async Task<IActionResult> UpdateAsync(Question question)
+        {
+            try
+            {
+                await questionService.UpdateAsync(question);
+
+                return Ok(question);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception);
+            }
+        }
+
+        public async Task<IActionResult> UpdateBulkAsync(IList<Question> questions)
+        {
+            try
+            {
+                await questionService.UpdateBulkAsync(questions);
+
+                return Ok(questions);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception);
+            }
+        }
+
+        public async Task<IActionResult> DeleteAsync(Guid id)
+        {
+            try
+            {
+                var question = await questionService.GetByIdAsync(id);
+
+                if(question is null)
+                {
+                    return NotFound();
+                }
+
+                await questionService.DeleteAsync(question);
+
+                return Ok(question);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception);
+            }
+        }
+
+        public async Task<IActionResult> DeleteBulkAsync(IList<Question> questions)
+        {
+            try
+            {
+                await questionService.DeleteBulkAsync(questions);
+
+                return Ok(questions);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception);
+            }
+        }
+    }
+}
