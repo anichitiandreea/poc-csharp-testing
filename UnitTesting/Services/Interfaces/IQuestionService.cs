0653f277d387862f26b947e6d0eae75999d6a7db
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-03-28 19:17:23 +0200

    Create QuestionService entity

---
+++ UnitTesting/Services/Interfaces/IQuestionService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnitTesting.Domains;
+
+namespace UnitTesting.Services.Interfaces
+{
+    public interface IQuestionService
+    {
+        Task<IList<Question>> GetAllAsync();
+        Task<Question> GetByIdAsync(Guid id);
+        Task CreateAsync(Question question);
+        Task CreateBulkAsync(IList<Question> questions);
+        Task UpdateAsync(Question question);
+        Task UpdateBulkAsync(IList<Question> questions);
+        Task DeleteAsync(Question question);
+        Task DeleteBulkAsync(IList<Question> questions);
+    }
+}
---
+++ UnitTesting/Services/QuestionService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnitTesting.Data;
+using UnitTesting.Domains;
+using UnitTesting.Services.Interfaces;
+
+namespace UnitTesting.Services
+{
+    public class QuestionService : IQuestionService
+    {
+        private readonly DatabaseContext context;
+
+        public QuestionService(DatabaseContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IList<Question>> GetAllAsync()
+        {
+            return await context.Questions.ToListAsync();
+        }
+
+        public async Task<Question> GetByIdAsync(Guid id)
+        {
+            return await context.Questions
+                .Where(question => question.Id == id
+                    && question.IsDeleted == false)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task CreateAsync(Question question)
+        {
+            context.Questions.Add(question);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task CreateBulkAsync(IList<Question> questions)
+        {
+            await context.Questions.AddRangeAsync(questions);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Question question)
+        {
+            context.Questions.Update(question);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateBulkAsync(IList<Question> questions)
+        {
+            context.Questions.UpdateRange(questions);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Question question)
+        {
+            context.Questions.Remove(question);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteBulkAsync(IList<Question> questions)
+        {
+            context.Questions.RemoveRange(questions);
+            await context.SaveChangesAsync();
+        }
+    }
+}
