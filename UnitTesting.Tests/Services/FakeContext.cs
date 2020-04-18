c258c98e0c35b4cc751cb2aeda7412c419d9dbef
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-04-19 00:35:05 +0300

    Create unit test for QuestionService using in memory database

---
+++ UnitTesting.Tests/Services/FakeContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using UnitTesting.Data;
+using UnitTesting.Domain;
+
+namespace UnitTesting.Tests.Services
+{
+    public class FakeContext
+    {
+        public DatabaseContext GetContextWithData()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                    .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                    .Options;
+            var context = new DatabaseContext(options);
+
+            context.Questions.Add(new Question
+            {
+                Id = Guid.NewGuid(),
+                UserId = Guid.NewGuid(),
+                Title = "Question1",
+                Description = "Description1",
+                IsDeleted = false,
+                Answers = null
+            });
+
+            context.Questions.Add(new Question
+            {
+                Id = Guid.NewGuid(),
+                UserId = Guid.NewGuid(),
+                Title = "Question2",
+                Description = "Description2",
+                IsDeleted = false,
+                Answers = null
+            });
+
+            context.Questions.Add(new Question
+            {
+                Id = Guid.NewGuid(),
+                UserId = Guid.NewGuid(),
+                Title = "Question3",
+                Description = "Description3",
+                IsDeleted = false,
+                Answers = null
+            });
+
+            context.SaveChanges();
+
+            return context;
+        }
+    }
+}
---
+++ UnitTesting.Tests/Services/QuestionServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnitTesting.Data;
+using UnitTesting.Domain;
+using UnitTesting.Services;
+
+namespace UnitTesting.Tests.Services
+{
+    public class QuestionServiceTest
+    {
+        [Test]
+        public async Task GivenGetAllAsyncWhenDataExistsThenReturnsData()
+        {
+            //Arrange
+            var context = new FakeContext().GetContextWithData();
+            var service = new QuestionService(context);
+
+            //Act
+            var result = await service.GetAllAsync();
+
+            //Assert
+            result.Should().BeOfType<List<Question>>();
+        }
+    }
+}
