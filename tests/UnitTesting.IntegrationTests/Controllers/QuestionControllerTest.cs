1e0fa0d6ea0a0eb81f9da62883910f71197bf4a8
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-05-07 19:12:37 +0300

    Add 2 working integration tests

--- tests/UnitTesting.IntegrationTests/Controllers/QuestionControllerTest.cs
+++ tests/UnitTesting.IntegrationTests/Controllers/QuestionControllerTest.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
+using UnitTesting.Data;
 using UnitTesting.Domain;
 using Xunit;
 
@@ -9,26 +12,52 @@ namespace UnitTesting.IntegrationTests.Controllers
 {
     public class QuestionControllerTest : IClassFixture<CustomWebApplicationFactory<Startup>>
     {
-        private readonly HttpClient _client;
-
+        private readonly HttpClient client;
         public QuestionControllerTest(CustomWebApplicationFactory<Startup> factory)
         {
-            _client = factory.CreateClient();
-        }
+            client = factory.CreateClient();
 
+            using (var scope = factory.Services.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var dbContext = scopedServices.GetRequiredService<DatabaseContext>();
+                if (dbContext.Database.EnsureCreated())
+                {
+                    SeedData.PopulateTestData(dbContext);
+                }
+            }
+        }
+        
         [Fact]
         public async Task GetAllAsync()
         {
             // The endpoint or route of the controller action.
-            var httpResponse = await _client.GetAsync("/question");
+            var httpResponse = await client.GetAsync("/question");
 
             // Must be successful.
             httpResponse.EnsureSuccessStatusCode();
 
             // Deserialize and examine results.
             var stringResponse = await httpResponse.Content.ReadAsStringAsync();
-            var players = JsonConvert.DeserializeObject<List<Question>>(stringResponse);
-            Assert.Contains(players, p => p.Title == "Wayne");
+            var questions = JsonConvert.DeserializeObject<List<Question>>(stringResponse);
+
+            Assert.Contains(questions, p => p.Title == "Wayne");
+        }
+
+        [Fact]
+        public async Task GetByIdAsync()
+        {
+            // The endpoint or route of the controller action.
+            var httpResponse = await client.GetAsync("/question/28066e80-31c4-4fdd-b8db-236af203b76d");
+
+            // Must be successful.
+            httpResponse.EnsureSuccessStatusCode();
+
+            // Deserialize and examine results.
+            var stringResponse = await httpResponse.Content.ReadAsStringAsync();
+            var question = JsonConvert.DeserializeObject<Question>(stringResponse);
+
+            question.Title.Should().Be("Wayne");
         }
     }
 }
